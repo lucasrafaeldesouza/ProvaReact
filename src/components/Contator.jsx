d781f2e98872bd8f7c31b778522bdaab640cfd6d
Fausto <faustortoloi@gmail.com>
2022-09-30 21:51:08 -0300

    renderização condicional

--- src/components/Contator.jsx
+++ src/components/Contator.jsx
@@ -1,9 +1,9 @@
-import { Button } from "@mui/material";
+import { Alert, Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 const Contador = () => {
   const [contador, setContador] = useState(11);
-  const [contadorAux, setContadorAux] = useState(0);
+  const [contadorAux, setContadorAux] = useState(false);
   const [mensagem, setMensagem] = useState("");
 
   const multiMes = () => {
@@ -16,21 +16,25 @@ const Contador = () => {
 
   useEffect(() => {
     if (contador > 12) {
-      setContadorAux(100);
+      setContadorAux(true);
       setMensagem("O ano possui apenas 12 meses");
+    } else {
+      setContadorAux(false);
     }
   }, [contador]);
 
-  useEffect(() => {
-    console.log("fui chamado useEffect Aux");
-  }, [contadorAux]);
+  useEffect(() => {}, [contadorAux]);
 
   return (
     <div>
       <div>Olá Contador</div>
       <div>Meu mês de aniversário é: {contador} </div>
       <div>
-        <div>{mensagem}</div>
+        {
+          /* Renderização condicional*/
+          contadorAux && <Alert severity="error">{mensagem}</Alert>
+        }
+
         <Button onClick={zerarMes}>Zera tudo!</Button>
         <Button onClick={multiMes}>Multiplica mês!</Button>
         <Button
