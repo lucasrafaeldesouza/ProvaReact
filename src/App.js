7aec59687f5bb037940ea73731771bb7c741467f
Fausto <faustortoloi@gmail.com>
2022-10-06 22:10:45 -0300

    aula 06/10/2022

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import logo from './logo.svg';
 import { Button, ButtonBase } from '@mui/material'
 import './App.css';
 import Contador from './components/Contator';
+import FetchNews from './components/FetchNews';
 
 
 function App() {
@@ -9,7 +9,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Olá Mundo</h1>
-        <Contador />      </header>
+        <div style={{ padding: "2rem" }}>
+          <FetchNews />
+          <Contador />
+        </div>
+      </header>
     </div>
   );
 }
