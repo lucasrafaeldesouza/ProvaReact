c78bdc1bb0d1a728ba769d62c316ed3418800af4
Fausto <faustortoloi@gmail.com>
2022-10-07 20:57:01 -0300

    aula 07-10-2022

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Button, ButtonBase } from '@mui/material'
 import './App.css';
 import Contador from './components/Contator';
 import FetchNews from './components/FetchNews';
+import FetchProducts from './components/FetchProducts';
 
 
 function App() {
@@ -10,8 +11,9 @@ function App() {
       <header className="App-header">
         <h1>Olá Mundo</h1>
         <div style={{ padding: "2rem" }}>
-          <FetchNews />
-          <Contador />
+          <FetchProducts />
+          {/* <FetchNews />
+          <Contador /> */}
         </div>
       </header>
     </div>
---
+++ src/components/FetchProducts.jsx
@@ -0,0 +1,61 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+const FetchProducts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const url = "https://dummyjson.com/products";
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((respData) => {
+        console.log(respData);
+        setData(respData.products);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div style={{display:'grid',gridTemplateColumns:"1fr 1fr 1fr", gap: "2rem"}}>
+      {isLoading ? (
+        <div>Carregando</div>
+      ) : (
+        data.map((produto) => (
+          <Card 
+            sx={{ maxWidth: 345 }} 
+            key={produto.id}
+            style={{marginBottom: "2rem"}}
+          >
+            <CardMedia
+              component="img"
+              height="140"
+              image={produto.thumbnail}
+              alt={product.brand}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {produto.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {produto.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Compartilhar</Button>
+              <Button size="small">Conheça mais</Button>
+            </CardActions>
+          </Card>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default FetchProducts;
